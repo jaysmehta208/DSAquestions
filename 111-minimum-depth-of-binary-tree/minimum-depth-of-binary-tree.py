0d4cbbe76b9cafcf21bc5b3c3f1d6cd5901b0933
Jay Mehta <jaysmehta@gmail.com>
2025-01-30 01:13:50 -0500

    Time: 43 ms (55.29%) | Memory: 50 MB (62.56%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -7,24 +7,25 @@
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
         
-        def dfs(temp, count, answer):
-            count+=1
-            if (temp.left == None and temp.right == None):
-                print("hello")
-                answer = min(count, answer)
-                return answer
-            a1 = 10**6
-            a2 = 10**6
-            if (temp.left != None):
-                a1 = dfs(temp.left, count, answer)
-            if (temp.right != None):
-                a2 = dfs(temp.right, count, answer)
-            return min(a1,a2)
-        answer = 10**6
-        temp = root
-        count = 0
-        if (temp == None):
-            return 0
-
-        answer = dfs(temp, count, answer)
-        return answer
+        # def dfs(temp, count, answer):
+        #     count+=1
+        #     if (temp.left == None and temp.right == None):
+        #         answer = min(count, answer)
+        #         return answer
+        #     a1 = 10**6
+        #     a2 = 10**6
+        #     if (temp.left != None):
+        #         a1 = dfs(temp.left, count, answer)
+        #     if (temp.right != None):
+        #         a2 = dfs(temp.right, count, answer)
+        #     return min(a1,a2)
+        def dfs(node):
+            if not node:
+                return 0
+            if not node.left:
+                return 1 + dfs(node.right)
+            if not node.right:
+                return 1 + dfs(node.left)
+            return 1 + min(dfs(node.left),dfs(node.right))
+        return dfs(root)
+       
