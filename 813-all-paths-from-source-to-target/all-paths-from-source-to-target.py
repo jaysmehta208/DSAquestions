5ca42909c3e11ac362d5dbfd3db06f8900781f2c
Jay Mehta <jaysmehta@gmail.com>
2025-01-30 00:09:45 -0500

    Time: 7 ms (43.79%) | Memory: 19.3 MB (21.93%) - LeetSync

---
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -0,0 +1,19 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        def dfs(value, graph, visited):
+            
+            if (value in visited):
+                return
+            visited.append(value)
+            if (value == len(graph) - 1):
+                answer.append(visited.copy())
+            
+            
+            for i in graph[value]:
+                dfs(i, graph, visited)
+            visited.pop()
+        visited = []
+        answer = []
+        dfs(0, graph, visited)
+        return answer
+
